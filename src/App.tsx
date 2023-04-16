b8787fa79e29acc26a55a4e663ff41fab10a8a5e
muslim <mhas2506@gmail.com>
2023-04-16 14:59:57 +0300

    organizing styles adding themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { Link, Route, Routes } from "react-router-dom";
+import { useThema } from "./theme/useThema";
 import { Suspense } from "react";
-import "./index.scss";
+import "./styles/index.scss";
 
 export const App = () => {
+  const { theme, handleThema } = useThema();
   return (
-    <>
+    <div className={`app ${theme}`}>
+      <button onClick={handleThema}>Theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О нас</Link>
       <Suspense fallback={<div>...Loading</div>}>
@@ -15,6 +18,6 @@ export const App = () => {
           <Route path={"/about"} element={<AboutPageAsync />} />
         </Routes>
       </Suspense>
-    </>
+    </div>
   );
 };
---
+++ src/theme/useThema.ts
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContex";
+
+interface UseThemaResult {
+    handleThema: () => void;
+    theme:Theme;
+}
+
+
+
+
+export function useThema():UseThemaResult {
+    const {theme, setTheme} = useContext(ThemeContext)
+    const handleThema = () => {
+    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK 
+
+      setTheme(newTheme);
+      localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+    }; 
+    return {
+        theme, handleThema
+    }
+}
