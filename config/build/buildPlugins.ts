71ac7d732d4d380d29dd9fd0174b458c62603e9f
muslim <mhas2506@gmail.com>
2023-04-18 17:09:43 +0300

    webpack, hot module replacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -15,6 +15,7 @@ export function buildPlugins({
       filename: "css/[name].[contenthash:8].css",
       chunkFilename: "css/[name].[contenthash:8].css",
     }),
-    new webpack.DefinePlugin({__IS_DEV__: JSON.stringify(isDev)})
+    new webpack.DefinePlugin({__IS_DEV__: JSON.stringify(isDev)}),
+    new webpack.HotModuleReplacementPlugin()
   ];
 }
