7f5b977995a7fb456f27f5acec30a361c9173f48
muslim <mhas2506@gmail.com>
2023-04-27 12:42:13 +0300

    bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -18,5 +19,6 @@ export function buildPlugins({
         }),
         new webpack.DefinePlugin({ __IS_DEV__: JSON.stringify(isDev) }),
         new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin(),
     ];
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/provider/themeProvider';
 import { App } from './app/App';
 import 'shared/config/i18n/i18n';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { ErrorBoundary } from './app/provider/ErrorBoundary';
 
 render(
