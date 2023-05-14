6ad23db250600e5a212fa03bf785c2f3f049bb3e
muslim <mhas2506@gmail.com>
2023-05-14 20:22:38 +0300

    upgrade sidebar

--- src/widgets/SideBar/ui/SideBar/SideBar.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.tsx
@@ -2,9 +2,11 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { Button, SizeButton, ThemeButton } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 
 interface SidebarProps {
     className?: string;
@@ -25,12 +27,24 @@ export const Sidebar = ({ className }: SidebarProps) => {
             <Button
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
+                className={cls.collapsedBtn}
+                theme={ThemeButton.BACKGROUND_INVERTED}
+                square
+                size={SizeButton.L}
             >
-                {t('Переключатель')}
+                {collapsed? ">" : "<"}
             </Button>
+
+
+            <div className={cls.items}><AppLink className={cls.link} theme={AppLinkTheme.SECONDARY} to={RoutePath.main}>
+                    {t('Главная')}
+                </AppLink>
+                <AppLink className={cls.link} theme={AppLinkTheme.RED} to={RoutePath.about}>
+                    {t('О сайте')}
+                </AppLink></div>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
+                <LangSwitcher short={collapsed} className={cls.lang} />
             </div>
         </div>
     );
