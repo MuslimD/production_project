07d1fc34c9c261c7ea7fbfcd8383633d08456bab
muslim <mhas2506@gmail.com>
2023-05-19 19:24:37 +0300

    screenshot test report

--- src/widgets/SideBar/ui/SideBar/SideBar.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.tsx
@@ -5,8 +5,10 @@ import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { Button, SizeButton, ThemeButton } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
-import cls from './Sidebar.module.scss';
+import AboutIcon from 'shared/assets/icons/about-20-20.svg';
+import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
     className?: string;
@@ -36,12 +38,28 @@ export const Sidebar = ({ className }: SidebarProps) => {
             </Button>
 
 
-            <div className={cls.items}><AppLink className={cls.link} theme={AppLinkTheme.SECONDARY} to={RoutePath.main}>
-                    {t('Главная')}
+            <div className={cls.items}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.main}
+                    className={cls.item}
+                >
+                    <MainIcon className={cls.icon} />
+                    <span className={cls.link}>
+                        {t('Главная')}
+                    </span>
                 </AppLink>
-                <AppLink className={cls.link} theme={AppLinkTheme.RED} to={RoutePath.about}>
-                    {t('О сайте')}
-                </AppLink></div>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.about}
+                    className={cls.item}
+                >
+                    <AboutIcon className={cls.icon} />
+                    <span className={cls.link}>
+                        {t('О сайте')}
+                    </span>
+                </AppLink>
+            </div>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher short={collapsed} className={cls.lang} />
